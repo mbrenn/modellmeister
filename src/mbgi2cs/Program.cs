facc774349e4c10966c42148101927444ae004f4
Martin Brenn <brenn@depon.net>
2014-12-26 11:03:14 -0500

    Visual Studio Solution is created

---
+++ src/mbgi2cs/Program.cs
@@ -0,0 +1,19 @@
+﻿using ModellMeister;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mbgi2cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Model Based Generation Instruction File to C#-Converter");
+
+            var converter = new Mbgi2CsConverter();
+        }
+    }
+}
