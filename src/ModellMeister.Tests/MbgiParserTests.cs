21d233791ed140cfc8ff813c6c0a662e6f060e34
Martin Brenn <brenn@depon.net>
2014-12-26 23:07:17 -0500

    Test for two types

--- src/ModellMeister.Tests/MbgiParserTests.cs
+++ src/ModellMeister.Tests/MbgiParserTests.cs
@@ -37,5 +37,44 @@ namespace ModellMeister.Tests
             Assert.That(currentType.Outputs[0].Name, Is.EqualTo("Sum"));
             Assert.That(currentType.Outputs[0].DataType, Is.EqualTo(DataType.Double));                
         }
+
+        [Test]
+        public void TestTwoFile()
+        {
+            var loadedFile = File.ReadAllText("../../../../examples/mbgi/twotypes.mbgi");
+
+            var parser = new MbgiFileParser();
+            var globalScope = parser.ParseFileFromText(loadedFile);
+
+            Assert.That(globalScope.Types.Count, Is.EqualTo(2));
+
+            var currentType = globalScope.Types[0];
+            Assert.That(currentType.Name, Is.EqualTo("Summer"));
+
+            Assert.That(currentType.Inputs.Count, Is.EqualTo(2));
+            Assert.That(currentType.Outputs.Count, Is.EqualTo(1));
+
+            Assert.That(currentType.Inputs[0].Name, Is.EqualTo("Summand1"));
+            Assert.That(currentType.Inputs[0].DataType, Is.EqualTo(DataType.Double));
+            Assert.That(currentType.Inputs[1].Name, Is.EqualTo("Summand2"));
+            Assert.That(currentType.Inputs[1].DataType, Is.EqualTo(DataType.Double));
+
+            Assert.That(currentType.Outputs[0].Name, Is.EqualTo("Sum"));
+            Assert.That(currentType.Outputs[0].DataType, Is.EqualTo(DataType.Double));
+
+            currentType = globalScope.Types[1];
+            Assert.That(currentType.Name, Is.EqualTo("Multiplier"));
+
+            Assert.That(currentType.Inputs.Count, Is.EqualTo(2));
+            Assert.That(currentType.Outputs.Count, Is.EqualTo(1));
+
+            Assert.That(currentType.Inputs[0].Name, Is.EqualTo("Factor1"));
+            Assert.That(currentType.Inputs[0].DataType, Is.EqualTo(DataType.Double));
+            Assert.That(currentType.Inputs[1].Name, Is.EqualTo("Factor2"));
+            Assert.That(currentType.Inputs[1].DataType, Is.EqualTo(DataType.Double));
+
+            Assert.That(currentType.Outputs[0].Name, Is.EqualTo("Product"));
+            Assert.That(currentType.Outputs[0].DataType, Is.EqualTo(DataType.Double));
+        }
     }
 }
