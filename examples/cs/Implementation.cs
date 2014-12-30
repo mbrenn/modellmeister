f6f7cbe5cd64a2f47e8746de96e15531105111da
Martin Brenn <brenn@depon.net>
2014-12-30 11:08:57 -0500

    Stylecop

--- examples/cs/Implementation.cs
+++ examples/cs/Implementation.cs
@@ -8,39 +8,44 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ModelBased {
-        
-    public partial class Sine {        
-        
-		private double time = 0.0;	
-        
+namespace ModelBased
+{
+
+    public partial class Sine
+    {
+
+        private double time = 0.0;
+
         partial void DoExecute()
-		{
-			time += 0.1;
-			this.Output = System.Math.Sin(time);
-		}
+        {
+            time += 0.1;
+            this.Output = System.Math.Sin(time);
+        }
     }
-    
-    public partial class Constant {        
-        
+
+    public partial class Constant
+    {
+
         partial void DoExecute()
-		{			
-			this.Output = 1;
-		}
+        {
+            this.Output = 1;
+        }
     }
-    
-    public partial class Adder {                
-        
+
+    public partial class Adder
+    {
+
         partial void DoExecute()
-		{
-			this.Sum = this.Summand1 + this.Summand2;
-		}
+        {
+            this.Sum = this.Summand1 + this.Summand2;
+        }
     }
-    
-    public partial class CSVWriter {
+
+    public partial class CSVWriter
+    {
         partial void DoExecute()
-		{
+        {
             System.Console.WriteLine(this.Input);
-		}
+        }
     }
 }
