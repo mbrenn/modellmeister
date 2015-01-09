4c129305270dac5b97073324da7e50e508e4756d
Martin Brenn <brenn@depon.net>
2015-01-08 23:08:57 -0500

    Included an example for the namespace

--- examples/build_examples.ps1
+++ examples/build_examples.ps1
@@ -5,6 +5,7 @@ Write-Output "Creates the C# files"
 ..\bin\mbgi2cs.exe ..\examples\mbgi\onlytype.mbgi ..\examples\cs\onlytype.cs
 ..\bin\mbgi2cs.exe ..\examples\mbgi\twotypes.mbgi ..\examples\cs\twotypes.cs
 ..\bin\mbgi2cs.exe ..\examples\mbgi\twoblocks.mbgi ..\examples\cs\twoblocks.cs
+..\bin\mbgi2cs.exe ..\examples\mbgi\twoblockswithnamespace.mbgi ..\examples\cs\twoblockswithnamespace.cs
 ..\bin\mbgi2cs.exe ..\examples\mbgi\fourblocks.mbgi ..\examples\cs\fourblocks.cs
 ..\bin\mbgi2cs.exe ..\examples\mbgi\compositeblock.mbgi ..\examples\cs\compositeblock.cs
 ..\bin\mbgi2cs.exe ..\examples\mbgi\autogenerationblock.mbgi ..\examples\cs\autogenerationblock.cs
@@ -19,6 +20,8 @@ Write-Output "- twotypes"
 ."$dotNetPath\csc.exe" /nologo /target:library /out:..\bin\twotypes.dll /debug+ /r:../../bin/ModellMeister.Runtime.dll /r:System.Runtime.dll twotypes.cs
 Write-Output "- twoblocks"
 ."$dotNetPath\csc.exe" /nologo /target:library /out:..\bin\twoblocks.dll /debug+ /r:../../bin/ModellMeister.Runtime.dll /r:System.Runtime.dll twoblocks.cs
+Write-Output "- twoblocks with namespace"
+."$dotNetPath\csc.exe" /nologo /target:library /out:..\bin\twoblocks.dll /debug+ /r:../../bin/ModellMeister.Runtime.dll /r:System.Runtime.dll twoblockswithnamespace.cs
 Write-Output "- fourblocks"
 ."$dotNetPath\csc.exe" /nologo /target:library /out:..\bin\fourblocks.dll /debug+ /r:../../bin/ModellMeister.Runtime.dll /r:System.Runtime.dll fourblocks.cs Implementation.cs
 Write-Output "- compositeblock"
