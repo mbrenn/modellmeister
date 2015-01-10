d8daeb094a7b41ec30384028ea77c8ddbb01fb57
Martin Brenn <brenn@depon.net>
2015-01-09 23:49:05 -0500

    Test is working again

--- src/ModellMeister.Tests/MbgiParserTests.cs
+++ src/ModellMeister.Tests/MbgiParserTests.cs
@@ -41,7 +41,6 @@ namespace ModellMeister.Tests
         [Test]
         public void TestTwoFile()
         {
-            throw new NotImplementedException("Break the test");
             var loadedFile = File.ReadAllText("../../../../examples/mbgi/twotypes.mbgi");
 
             var parser = new MbgiFileParser();
