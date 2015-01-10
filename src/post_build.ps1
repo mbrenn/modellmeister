8903ec2b19195db9124e905aeba1342209fee389
Martin Brenn <brenn@depon.net>
2015-01-09 22:49:09 -0500

    Bin directory will be created

--- src/post_build.ps1
+++ src/post_build.ps1
@@ -1,2 +1,9 @@
 Write-Host "Copy files to the bin-directory"
+
+if (-Not (Test-Path "$($args[2])../bin")) {
+	New-Item -ItemType directory -Path "$($args[2])../bin"
+}
+
+
+Write-Host "$($args[1])*.*" "$($args[2])..\bin\"
 Copy-Item "$($args[1])*.*" "$($args[2])..\bin\"
